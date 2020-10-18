b1f831bba776e7e45a19154c60045d798b40e673
Satan <patrik__96@hotmail.com>
2020-10-18 17:08:27 +0200

    Update main.cpp
    Added try catch block. 
    
    You only get 1000 calls per month, and the json lib REALLY doesn't like non-try's

--- main.cpp
+++ main.cpp
@@ -10,25 +10,37 @@ int main(int argc, char **argv)
         city = argv[2];
     }
     httplib::Client cli("http://api.weatherstack.com");
-    auto res = cli.Get((std::string("/current?access_key=XXXXXXXXXXXXXXXXX&query=") + city).c_str());
+    auto res = cli.Get((std::string("/current?access_key=XXXXXXXXXXXXXXXX&query=") + city).c_str());
 
     if(res->status == 200)
     {
         auto j = json::parse(res->body);
-        unsigned temp = j.at("current").at("temperature");
-        std::string city = j.at("location").at("name");
-        std::string weather = j.at("current").at("weather_descriptions").at(0);
-        unsigned feels = j.at("current").at("feelslike");
-        std::string time = j.at("location").at("localtime");
-        time.erase(0, 11);
-        std::string wind_dir = j.at("current").at("wind_dir");
-        unsigned wind_speed = j.at("current").at("wind_speed");
-        std::cout << "Time: " << time << std::endl; // Technically cheating
-        std::cout << "Temperature: " << temp << "c" << std::endl;
-        std::cout << "Feels-like: " << feels << "c" <<std::endl; 
-        std::cout << "Weather: " << weather << std::endl;
-        std::cout << "Wind: " << wind_speed << "m/s, " << wind_dir << std::endl;
-        std::cout << "Location: " << city << std::endl;
+        try 
+        {
+            unsigned temp = j.at("current").at("temperature");
+            std::string city = j.at("location").at("name");
+            std::string weather = j.at("current").at("weather_descriptions").at(0);
+            unsigned feels = j.at("current").at("feelslike");
+            std::string time = j.at("location").at("localtime");
+            time.erase(0, 11);
+            std::string wind_dir = j.at("current").at("wind_dir");
+            unsigned wind_speed = j.at("current").at("wind_speed");
+            std::cout << "Observation time: " << time << std::endl; // Technically cheating
+            std::cout << "Temperature: " << temp << "c" << std::endl;
+            std::cout << "Feels-like: " << feels << "c" <<std::endl; 
+            std::cout << "Weather: " << weather << std::endl;
+            std::cout << "Wind: " << wind_speed << "m/s, " << wind_dir << std::endl;
+            std::cout << "Location: " << city << std::endl;
+        } catch (const std::exception& e) 
+        {
+            std::cout << "Time: " << "N/A" << std::endl;
+            std::cout << "Temperature: " << "N/A" << "c" << std::endl;
+            std::cout << "Feels-like: " << "N/A" << "c" <<std::endl; 
+            std::cout << "Weather: " << "N/A" << std::endl;
+            std::cout << "Wind: " << "N/A " << "m/s" << std::endl;
+            std::cout << "Location: " << "N/A" << std::endl;
+            return EXIT_FAILURE;
+        }
     }
     else
     {
